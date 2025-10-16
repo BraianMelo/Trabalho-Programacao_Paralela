637e09aa95cff97fd252d3d48bc8e618e993ea7e
Braian <braian.ptu@gmail.com>
2025-10-16 20:17:54 -0300

    fix: lista vai de 2 até N

--- src/Lista.c
+++ src/Lista.c
@@ -3,7 +3,7 @@
 int* criar_lista(int n) {
 	int *lista = malloc(n * sizeof(int));
 	
-	for(int i = 0; i < n; ++i) {
+	for(int i = 0; i < (n - 1); ++i) {
 		lista[i] = i + 2;
 	}
 	
@@ -11,7 +11,7 @@ int* criar_lista(int n) {
 }
 
 void imprimir_lista(int *lista, int n) {
-	for(int i = 0; i < n; ++i) {
+	for(int i = 0; i < (n - 1); ++i) {
 		printf("Lista[%d] = %d \n", i, lista[i]);
 	}
 }
