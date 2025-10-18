37d35a1aacf5dd312d35308b8d38b94c2bcf5bb5
Braian <braian.ptu@gmail.com>
2025-10-18 18:29:14 -0300

    feat: modo sendo passado como parâmetro

--- src/Utilitarios.h
+++ src/Utilitarios.h
@@ -5,7 +5,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int checar_parametros(int argc, char *argv[]);
+int checar_parametros(int argc, char *argv[], char *modo);
 bool checar_N(int n);
+bool checar_modo(char modo);
 
 #endif
